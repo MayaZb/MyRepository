1ea0fa2c1b183e08486381c6a10b3c60e01a52c4
Maiia Zbitnieva <mayazbt@gmail.com>
2020-04-06 18:16:21 +0300

    github

--- lesson_22/main.js
+++ lesson_22/main.js
@@ -30,17 +30,3 @@
             }
         });
     });
-
-    /*$(document).ready(function() {
-        $('.spoiler_title').click(function() { // при клике по заголовку спойлера
-            var show = $(this).attr('show'); // проверяем атрибут, в котором записано - отображен спойлер или скрыт
-
-            if (show == 1) { // если отображен, то прячем
-                $(this).attr('show', 0);
-                $('.spoiler_block').slideUp(300);
-            } else { // если спрятан, то показываем
-                $(this).attr('show', 1);
-                $('.spoiler_block').slideDown(300);
-            }
-        });
-    }); */
