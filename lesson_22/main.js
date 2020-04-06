090ac0df9464dc11b9c1070b3add2c2c861a48e4
Maiia Zbitnieva <mayazbt@gmail.com>
2020-04-06 18:11:00 +0300

    github

--- lesson_22/main.js
+++ lesson_22/main.js
@@ -1,36 +1,27 @@
     $('#up').click(function() {
         $('html, body').animate({ scrollTop: 0 }, 500);
         return false;
-    })
+    });
 
     $('#down').click(function() {
         $('html, body').animate({ scrollTop: $("#elem").offset().top }, 500);
         return false;
-    })
+    });
 
-    window.onscroll = function vverh() {
-        document.getElementById('up').style.display = (window.pageYOffset > '500' ? 'block' : 'none');
-    }
+    //кнопка
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > 500) {
+            $('#up').show();
+        } else {
+            $('#up').hide();
+        }
+    });
 
-    /*$(document).ready(function() {
-        $('.spoiler_title').click(function() { // при клике по заголовку спойлера
-            var show = $(this).attr('show'); // проверяем атрибут, в котором записано - отображен спойлер или скрыт
-            if (show == 1) { // если отображен, то прячем
-                $(this).attr('show', 0);
-                $('.spoiler_block').slideUp(300);
-            } else { // если спрятан, то показываем
-                $(this).attr('show', 1);
-                $('.spoiler_block').slideDown(300);
-            }
-        });
-    });*/
-
-
+    //спойлер
     $(document).ready(function() {
         $('.spoiler_title').click(function() { // при клике по заголовку спойлера
-            var show = $(this).attr('show'); // проверяем атрибут, в котором записано - отображен спойлер или скрыт
-
-            if (show == 1) { // если отображен, то прячем
+            let $show1 = $(this).attr('show');
+            if ($show1 == 1) { // если отображен, то прячем
                 $(this).attr('show', 0);
                 $('.spoiler_block').slideUp(300);
             } else { // если спрятан, то показываем
@@ -39,3 +30,17 @@
             }
         });
     });
+
+    /*$(document).ready(function() {
+        $('.spoiler_title').click(function() { // при клике по заголовку спойлера
+            var show = $(this).attr('show'); // проверяем атрибут, в котором записано - отображен спойлер или скрыт
+
+            if (show == 1) { // если отображен, то прячем
+                $(this).attr('show', 0);
+                $('.spoiler_block').slideUp(300);
+            } else { // если спрятан, то показываем
+                $(this).attr('show', 1);
+                $('.spoiler_block').slideDown(300);
+            }
+        });
+    }); */
